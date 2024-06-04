92139f11c303de398fd73089493d8ccc54c42033
Mahnoor <mahnoort104@gmail.com>
2024-06-04 12:37:41 +0500

    Add files via upload

---
+++ timer.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+
+import inquirer from 'inquirer';
+
+async function startTimer() {
+    const { duration } = await inquirer.prompt({
+        type: 'number',
+        name: 'duration',
+        message: 'Enter the duration of the countdown timer (in seconds):',
+    });
+
+    console.log('Countdown started!');
+
+    let remainingTime = duration;
+
+    const interval = setInterval(() => {
+        remainingTime--;
+
+        if (remainingTime === 0) {
+            clearInterval(interval);
+            console.log('Countdown finished!');
+        } else {
+            console.log(`Time remaining: ${remainingTime} seconds`);
+        }
+    }, 1000);
+}
+
+startTimer();
